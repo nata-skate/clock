2eafa629fe68314749b9e19b77bf62e08eba2c03
nata-skate <49261319+nata-skate@users.noreply.github.com>
2019-06-07 22:12:15 +0800

    备忘录页面大致完成了

--- pages/note/note.js
+++ pages/note/note.js
@@ -1,66 +1,58 @@
 // pages/note/note.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    list: [{
+      id: 1,
+      name: '',
+      count: 1
+    }, {
+      id: 2,
+      name: '',
+      count: 6
+    }, {
+      id: 3,
+      name: '',
+      count: 12
+    }]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //向后增加数据
+  add_after: function () {
+    var newarray = [{
+      id: 5,
+      name:'',
+      count: 89
+    }];
+    this.setData({
+      'list': this.data.list.concat(newarray)
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+inputnote:function(e){
+  var dataset = e.target.dataset;
+  var index = dataset.index; //拿到是第几个数组(这里是模仿删除的写的。。。应该能拿到下标吧)
+  this.data.list[index].name = e.detail.value;
+  this.setData({
+    list: this.data.list
+  })
+},
 
+  //删除
+  remove: function (e) {
+    var dataset = e.target.dataset;
+    var Index = dataset.index; //拿到是第几个数组
+    this.data.list.splice(Index, 1);
+    this.setData({
+      list: this.data.list
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //清空
+  clear: function () {
+    this.setData({
+      list: []
+    });
   }
+
 })
